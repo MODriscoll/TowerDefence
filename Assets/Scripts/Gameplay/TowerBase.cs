4ac4caa4239ee5a1fa4a2da60f01e56ab22124f1
Michael O'Driscoll <MODriscoll55@outlook.com>
2020-02-13 23:12:30 +1100

    Fixed tower rotation and implementation for following a target. Started outlining the monster class.
    Still continuing investigation into Photon

--- Assets/Scripts/Gameplay/TowerBase.cs
+++ Assets/Scripts/Gameplay/TowerBase.cs
@@ -3,28 +3,103 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
+// Temporary location
+public class MonsterBase : MonoBehaviourPunCallbacks
+{
+
+}
+
+// Temporary location
+public class MonsterManager
+{
+    // Pointer to manager currently in use, we use this
+    // as we want to find monsters but not through collision
+    public static MonsterManager manager;
+
+    /// <summary>
+    /// Get all monsters within radius of position
+    /// </summary>
+    /// <param name="position">Position to check</param>
+    /// <param name="radius">Radius of check</param>
+    /// <returns>List of monsters or null if no monsters in radius</returns>
+    public List<MonsterBase> getMonstersInRadius(Vector2 position, float radius)
+    {
+        float radSqr = radius * radius;
+
+        // Iterate each monster and add all monsters within sqr distance to list
+        List<MonsterBase> foundMonsters = new List<MonsterBase>();
+        foreach (MonsterBase monster in m_monsters)
+        {
+            Vector2 dis = (Vector2)monster.transform.position - position;
+            if (dis.sqrMagnitude <= radSqr)
+            {
+                foundMonsters.Add(monster);
+            }
+        }
+
+        // Return null to signal no monsters in range
+        if (foundMonsters.Count > 0)
+            return foundMonsters;
+        else
+            return null;
+    }
+
+    /// <summary>
+    /// Get the monster closest to position that is still within the given radius
+    /// </summary>
+    /// <param name="position">Position to check</param>
+    /// <param name="radius">Radius monster must be in</param>
+    /// <returns>Closest monster or null if no monster is in range</returns>
+    public MonsterBase getClosestMonsterTo(Vector2 position, float radius = 1000f)
+    {
+        float radSqr = radius * radius;
+
+        // The current closest monster and its distance from position (squared)
+        MonsterBase closest = null;
+        float closestDis = float.MaxValue;
+
+        // Iterate each monster and find the closest one
+        foreach (MonsterBase monster in m_monsters)
+        {
+            Vector2 dis = (Vector2)monster.transform.position - position;
+
+            // Monster needs to be in radius for it to be considered
+            float magSqr = dis.sqrMagnitude;
+            if (magSqr <= radSqr && magSqr < closestDis)
+            {
+                closest = monster;
+                closestDis = magSqr;
+            }
+        }
+
+        return closest;
+    }
+
+    private List<MonsterBase> m_monsters;
+}
+
+[RequireComponent(typeof(PhotonView))]
 public class TowerBase : MonoBehaviourPunCallbacks
 {
-    public float m_targetRadius = 2.5f;
-
-    // temp
-    public Camera m_Camera;
+    public float m_targetRadius = 2.5f;     // Radius the tower can see
+    public Transform m_Target;              // The target for use to look at
 
     private PhotonView m_networkView;
 
     void Awake()
     {
+        MonsterManager.manager = new MonsterManager();
         m_networkView = GetComponent<PhotonView>();
     }
 
     void Update()
     {
-        //if (!m_networkView.IsMine)
-        //{
-        //    return;
-        //}
+        if (!m_Target)
+        {
+            return;
+        }
 
-        Vector2 mousePos = GetTargetLocation();
+        Vector2 mousePos = m_Target.position;
         Vector3 eulerRot = Vector3.zero;
 
         Vector2 pos = new Vector2(transform.position.x, transform.position.z);
@@ -35,21 +110,16 @@ public class TowerBase : MonoBehaviourPunCallbacks
         }
 
         dir.Normalize();
-        float x = dir.x;
-        float y = dir.y;
-        eulerRot.z = Mathf.Atan2(y, x);
+        eulerRot.z = Mathf.Rad2Deg * Mathf.Atan2(dir.y, dir.x);
 
         transform.eulerAngles = eulerRot;
     }
 
-    public Vector2 GetTargetLocation()
+    protected MonsterBase findTarget(float radius)
     {
-        Vector2 mousePos = Input.mousePosition;
-        if (m_Camera)
-        {
-            mousePos = m_Camera.transform.TransformPoint(m_Camera.ScreenToWorldPoint(Input.mousePosition));
-        }
-
-        return mousePos;
+        if (MonsterManager.manager != null)
+            return MonsterManager.manager.getClosestMonsterTo(transform.position);
+        else
+            return null;
     }
 }
