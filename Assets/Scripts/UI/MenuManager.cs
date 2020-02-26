42e2e2541e3c3ee20d917aff61e6e2a81281b357
Max Heins <48420290+Spluppo@users.noreply.github.com>
2020-02-26 14:56:51 +1100

    Implemented Main Menu and Alpha Game UI
    The main menu can navigate between screens, which still need to be populated. The game UI still needs to connected to turret/minion spawning now.

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+    public GameObject shopMenu;
+    public GameObject matchTypeMenu;
+    public GameObject privateMatchMenu;
+    static private GameObject[] menus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menus = new GameObject[5] { mainMenu, matchTypeMenu, optionsMenu, shopMenu, privateMatchMenu };
+    }
+
+    public void LoadMenu(string newMenu)
+    {
+        foreach (GameObject menu in menus)
+        {
+            if (menu.name == newMenu)
+                menu.SetActive(true);
+            else
+                menu.SetActive(false);
+        }
+    }
+
+
+    public void LoadScene(string scene)
+    {
+        LoadScene(scene);
+    }
+}
