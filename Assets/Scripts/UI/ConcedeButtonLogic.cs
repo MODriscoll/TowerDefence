89ffe2c6fee2c0d28e21ad5c044e8d37f0ea1275
Max Heins <48420290+Spluppo@users.noreply.github.com>
2020-03-06 15:19:48 +1100

    Adjusted Game and Options UI
    Added a concede button and bulldoser button (it won't destroy anything though)

---
+++ Assets/Scripts/UI/ConcedeButtonLogic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConcedeButtonLogic : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+            gameObject.SetActive(true);
+        else
+            gameObject.SetActive(false);
+    }
+
+    public void ExitToMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
