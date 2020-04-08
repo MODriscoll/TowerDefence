8b32c89ab71daa4d246ad0ee05d9721f0d21a70b
Max <madmanmax120@hotmail.com>
2020-04-08 19:22:38 +1000

    Tweaked Matchmaking Menu
    Added labels to each of the input fields.

--- Assets/Scripts/Gameplay/Aesthetics/VolumeManager.cs
+++ Assets/Scripts/Gameplay/Aesthetics/VolumeManager.cs
@@ -5,21 +5,30 @@ using UnityEngine.UI;
 
 public class VolumeManager : MonoBehaviour
 {
+    [Range(0, 1)] public float m_masterVolume;
     [Range(0, 1)] public float m_musicVolume;
     [Range(0, 1)] public float m_sfxVolume;
 
+    public Slider masterSlider;
     public Slider musicSlider;
     public Slider sfxSlider;
 
     private AudioSource musicAudioSource;
-    private AudioSource sfxAudioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         musicAudioSource = FindObjectOfType<AudioSource>();
-        musicAudioSource.volume = PlayerPrefs.GetFloat("MusicVolume");
-        sfxAudioSource.volume = PlayerPrefs.GetFloat("SFXVolume");
+        UpdateMasterVolume();
+        UpdateMusicVolume();
+        UpdateSFXVolume();
+    }
+
+    public void UpdateMasterVolume()
+    {
+        m_masterVolume = masterSlider.value;
+        musicAudioSource.volume = m_musicVolume * m_masterVolume;
+        PlayerPrefs.SetFloat("MasterVolume", m_masterVolume);
     }
 
     public void UpdateMusicVolume()
@@ -32,7 +41,6 @@ public class VolumeManager : MonoBehaviour
     public void UpdateSFXVolume()
     {
         m_sfxVolume = sfxSlider.value;
-        sfxAudioSource.volume = m_sfxVolume;
         PlayerPrefs.SetFloat("SFXVolume", m_sfxVolume);
     }
 }
