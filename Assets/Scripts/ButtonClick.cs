24fbf008cdd84506ea426047d0e59aa75755248b
Max <madmanmax120@hotmail.com>
2020-04-22 17:34:30 +1000

    Updated Main Menu Background + Updated Options
    Main Menu Background got an overhaul and looks way better.
    Options Menu got fixed and now works 100% as intended!

--- Assets/Scripts/ButtonClick.cs
+++ Assets/Scripts/ButtonClick.cs
@@ -10,18 +10,25 @@ public class ButtonClick : MonoBehaviour
 
     private void Start()
     {
-        if (audioSource == null)
-            audioSource = FindObjectOfType<AudioSource>();
+        GetAudioSource();
     }
 
     public void PlaySound()
     {
+        GetAudioSource();
+        audioSource.PlayOneShot(ClickSound);
+    }
+
+    private void GetAudioSource()
+    {
         if (audioSource == null)
         {
-            Debug.Log("No AudioSource found, relocating...");
-            audioSource = FindObjectOfType<AudioSource>();
+            foreach (AudioSource source in FindObjectsOfType<AudioSource>())
+            {
+                if (source.name.Contains("SFX"))
+                    audioSource = source;
+            }
         }
-        audioSource.PlayOneShot(ClickSound);
     }
 
 }
--- Assets/Scripts/Gameplay/Aesthetics/VolumeManager.cs
+++ Assets/Scripts/Gameplay/Aesthetics/VolumeManager.cs
@@ -9,38 +9,83 @@ public class VolumeManager : MonoBehaviour
     [Range(0, 1)] public float m_musicVolume;
     [Range(0, 1)] public float m_sfxVolume;
 
+    public Toggle toggleMaster;
+    public Toggle toggleMusic;
+    public Toggle toggleSFX;
+
     public Slider masterSlider;
     public Slider musicSlider;
     public Slider sfxSlider;
 
     private AudioSource musicAudioSource;
+    private AudioSource sfxAudioSource;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        musicAudioSource = FindObjectOfType<AudioSource>();
-        UpdateMasterVolume();
-        UpdateMusicVolume();
-        UpdateSFXVolume();
+        // Set AudioSources to their corresponding source
+        foreach (AudioSource source in FindObjectsOfType<AudioSource>())
+        {
+            if (source.name.Contains("Music"))
+                musicAudioSource = source;
+            else if (source.name.Contains("SFX"))
+                sfxAudioSource = source;
+        }
+        
+        StartSequence();
+        gameObject.SetActive(false);
     }
 
-    public void UpdateMasterVolume()
+    public void StartSequence()
+    {
+        toggleMaster.isOn = (PlayerPrefs.GetInt("MasterToggle") > 0) ? false : true;
+        toggleMusic.isOn = (PlayerPrefs.GetInt("MusicToggle") > 0) ? false : true;
+        toggleSFX.isOn = (PlayerPrefs.GetInt("SFXToggle") > 0) ? false : true;
+
+        masterSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+
+        SetMasterVolume();
+        SetMusicVolume();
+        SetSFXVolume();
+    }
+
+    public void ToggleMaster()
+    {
+        UpdateToggle("MasterToggle");
+        SetMasterVolume();
+    }
+    public void ToggleMusic()
+    {
+        UpdateToggle("MusicToggle");
+        SetMusicVolume();
+    }
+    public void ToggleSFX()
+    {
+        UpdateToggle("SFXToggle");
+        SetSFXVolume();
+    }
+    public void UpdateToggle(string id)
+    {
+        int state = Mathf.Abs(PlayerPrefs.GetInt(id) - 1);
+        PlayerPrefs.SetInt(id, state);
+    }
+
+    public void SetMasterVolume()
     {
         m_masterVolume = masterSlider.value;
-        musicAudioSource.volume = m_musicVolume * m_masterVolume;
-        PlayerPrefs.SetFloat("MasterVolume", m_masterVolume);
+        SetMusicVolume();
+        SetSFXVolume();
     }
-
-    public void UpdateMusicVolume()
+    public void SetMusicVolume()
     {
         m_musicVolume = musicSlider.value;
-        musicAudioSource.volume = m_musicVolume;
-        PlayerPrefs.SetFloat("MusicVolume", m_musicVolume);
+        musicAudioSource.volume = m_masterVolume * m_musicVolume * PlayerPrefs.GetInt("MasterToggle") * PlayerPrefs.GetInt("MusicToggle");
     }
-
-    public void UpdateSFXVolume()
+    public void SetSFXVolume()
     {
         m_sfxVolume = sfxSlider.value;
-        PlayerPrefs.SetFloat("SFXVolume", m_sfxVolume);
+        sfxAudioSource.volume = m_masterVolume * m_sfxVolume * PlayerPrefs.GetInt("MasterToggle") * PlayerPrefs.GetInt("SFXToggle");
     }
 }
